ce84fd7511587389fe887056196ba9b599d23bb9
Levi <ljheidrick@gmail.com>
2017-09-10 10:28:07 -0500

    creating calendar view for managing workouts

--- whatsmyworkout-vue/src/components/manage-workouts.vue
+++ whatsmyworkout-vue/src/components/manage-workouts.vue
@@ -1,6 +1,6 @@
 <template>
     <v-layout row>
-        <v-flex md5 xs12>
+        <v-flex md6 xs12>
             <v-card>
                 <v-card-title primary-title>
                     <div>
@@ -9,6 +9,51 @@
                     </div>
                 </v-card-title>
                 <v-divider></v-divider>
+                <v-layout class="ml-0 mr-0">
+                <v-flex md4 class="pl-0 pr-0">
+                    <div style="border-right: solid 1px black; border-bottom: 1px solid black; height: 100px;">
+                        <span style="display: inline-block" class="pa-2">4</span>
+                    </div>
+                </v-flex>
+
+                <v-flex md4 class="pl-0 pr-0">
+                    <div style="border-right: solid 1px black; border-bottom: 1px solid black; height: 100px;">
+                        <span style="display: inline-block" class="pa-2">5</span>
+                    </div>
+                </v-flex>
+                <v-flex md4 class="pl-0 pr-0">
+                    <div style="border-bottom: 1px solid black; height: 100px;">
+                        <span style="display: inline-block" class="pa-2">6</span>
+                    </div>
+                </v-flex>
+                    </v-layout>
+                            <v-layout class="ml-0 mr-0">
+                <v-flex md4 class="pl-0 pr-0">
+                    <div style="border-bottom: 1px solid black; border-right: solid 1px black; height: 100px;">
+                        <div>
+                            <span style="display: inline-block" class="pa-2">7</span>
+                        </div>
+                    </div>
+                </v-flex>
+
+                <v-flex md4 class="pl-0 pr-0">
+                    <div style="border-right: solid 1px black; border-bottom: 1px solid black; height: 100px;">
+                        <span style="display: inline-block" class="pa-2">8</span>
+                    </div>
+                </v-flex>
+                <v-flex md4 class="pl-0 pr-0">
+                    <div  style="height: 100px; border-bottom: 1px solid black;">
+                        <span style="display: inline-block" class="pa-2">9</span>
+                    </div>
+                </v-flex>
+                    </v-layout>
+                <v-layout class="ml-0 mr-0">
+                    <v-flex md4 xs4 class="pl-0 pr-0">
+                        <div  style="height: 100px;  border-right: 1px solid black;">
+                        <span style="display: inline-block" class="pa-2">10</span>
+                    </div>
+                    </v-flex>
+                </v-layout>
             </v-card>
         </v-flex>
     </v-layout>
