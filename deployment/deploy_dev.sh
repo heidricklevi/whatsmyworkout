bacf1191cb6d43a8df43375e3342bd73a58ffcbd
Levi <ljheidrick@gmail.com>
2018-04-05 14:19:18 -0500

    added requirements.txt to git

--- deployment/deploy_dev.sh
+++ deployment/deploy_dev.sh
@@ -4,6 +4,6 @@ cd /var/www/whatsmyworkout
 git pull https://github.com/heidricklevi/whatsmyworkout.git
 cd whatsmyworkout-vue
 npm run build
-yes | \cp -rf dist ../account/static/
+yes | sudo \cp -rf dist ../account/static/
 cd ..
 python3 manage.py collectstatic
