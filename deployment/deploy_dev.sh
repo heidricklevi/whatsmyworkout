2ad13282888eaa078a5cfe71fb6b9c361d6cd849
Levi <ljheidrick@gmail.com>
2018-04-05 14:09:08 -0500

    test run with auto builds and deploy to dev environment

--- deployment/deploy_dev.sh
+++ deployment/deploy_dev.sh
@@ -4,7 +4,6 @@ cd /var/www/whatsmyworkout
 git pull https://github.com/heidricklevi/whatsmyworkout.git
 cd whatsmyworkout-vue
 npm run build
-chmod 777 dist
-cp -r dist ../account/static/
+yes | \cp -rf dist ../account/static/
 cd ..
 python3 manage.py collectstatic
