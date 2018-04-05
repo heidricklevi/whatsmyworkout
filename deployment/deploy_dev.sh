7ddf7c4b796a0eaf4c5bf0fb66e250b76c4f2866
Levi <ljheidrick@gmail.com>
2018-04-05 13:51:03 -0500

    test run with auto builds and deploy to dev environment

--- deployment/deploy_dev.sh
+++ deployment/deploy_dev.sh
@@ -4,6 +4,6 @@ cd /var/www/whatsmyworkout
 git pull https://github.com/heidricklevi/whatsmyworkout.git
 cd whatsmyworkout-vue
 npm run build
-cp dist ../account/static/
+cp -r dist ../account/static/
 cd ..
 python3 manage.py collectstatic
