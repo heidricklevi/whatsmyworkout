f2eef14c1715d7a6c709079c17217234384976c6
Levi H <ljheidrick@gmail.com>
2019-08-24 12:27:12 -0500

    adjust to get correct css version of vuetify styles working again

--- whatsmyworkout-vue/src/main.js
+++ whatsmyworkout-vue/src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vuetify from 'vuetify'
-import 'vue-event-calendar/dist/style.css' //^1.1.10, CSS has been extracted as one file, so you can easily update it.
 import vueEventCalendar from 'vue-event-calendar'
 import Vuex from 'vuex'
 import store from './store'
 
+Vue.use(vueEventCalendar, {
+    locale: 'en'
+  }
+);
+Vue.use(Vuex);
 
-
-Vue.use(vueEventCalendar, {locale: 'en'});
 Vue.use(Vuetify, {
   theme: {
     primary: '#607D8B',
@@ -18,9 +20,6 @@ Vue.use(Vuetify, {
     success: '#0288D1'
   }
 });
-Vue.use(Vuex);
-
-
 
 
 new Vue({
